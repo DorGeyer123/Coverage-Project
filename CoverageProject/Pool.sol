c1983f3df9dd9b957e10d60d3b63194f78f58a8d
DorGeyer123 <91005347+DorGeyer123@users.noreply.github.com>
2021-12-28 16:29:47 +0200

    co

--- CoverageProject/Pool.sol
+++ CoverageProject/Pool.sol
@@ -10,35 +10,21 @@ contract Pool is LPToken {
 
     function withdraw(uint256 amount) public returns (bool success)  {
 		_burn(msg.sender,amount);
-		success = payable(msg.sender).send(amount);
+		success = payable(msg.sender).send((amount/totalSupply)*address(this).balance);
 		totalSupply-=amount;
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-function FlashLoan(address asset, address receiverAddress, uint256 amount) public {
-=======
     
     function FlashLoan(address asset, address receiverAddress, uint256 amount) public {
->>>>>>> 7bcf79268e219cedd849462b11feb346b817b714
     address receiver = IFlashLoanReceiver(receiverAddress);
     uint256 totalPremium = (amount*18)/10000;
     uint256 amountPlusPremium = amount + totalPremium;
     IERC20(asset).safeTransferFrom(address(this),receiverAddress,amount);
     require(receiver.executeOperation(asset,amount,totalPremium,msg.sender),'P_INVALID_FLASH_LOAN_EXECUTOR_RETURN');
-    IERC20(params.asset).safeTransferFrom(receiverAddress,address(this),amountPlusPremium);
+    IERC20(asset).safeTransferFrom(receiverAddress,address(this),amountPlusPremium);
   }
-<<<<<<< HEAD
  
-=======
-	
->>>>>>> 4740b133a5921f01b8e808652427afc7a33b26f3
-	
-	function getEthBalance(address account) public view returns (uint256){
-=======
-	
     function getEthBalance(address account) public view returns (uint256){
->>>>>>> 7bcf79268e219cedd849462b11feb346b817b714
 		return account.balance;
 	}
 
