d11bdf64376fd7e7c99e10f6bf5e4c7bb10217f0
Gadi Rechlis <gadi@certora.com>
2022-01-02 23:45:39 +0200

    script

--- CoverageProject/script.sh
+++ CoverageProject/script.sh
@@ -1,3 +1,5 @@
-certoraRun Pool.sol LP_ERC20.sol  \
+certoraRun Pool.sol LP_ERC20.sol ASSET_  \
 	--verify Pool:spec.spec \
-    --solc solc8.0
+    --solc solc8.11 \
+    --output ../output \
+    --rule $1
